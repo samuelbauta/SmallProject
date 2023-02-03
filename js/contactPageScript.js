4a8c76a61918d9c0fc8359e7c43f53742ec22bff
jevans556 <47763236+jevans556@users.noreply.github.com>
2023-02-02 21:54:13 -0500

    Add files via upload

--- js/contactPageScript.js
+++ js/contactPageScript.js
@@ -1,77 +1,170 @@
-const urlBase = 'LAMPAPI/'
+const urlBase = 'http://cop4331.site/LAMPAPI'
 const extension = 'php'
 let userID = 0
 let firstName = ""
 let lastName = ""
+const ids = [];
 
 document.addEventListener('DOMContentLoaded', function () {
     readCookie()
 }, false)
-alert("Hello " + firstName)
 
-document.getElementById("addContact").addEventListener("click", addContact);
+document.getElementById("addNewContact").addEventListener("click", addContact);
 
-function addContact() {
+function loadContact()
+{
+    let temp = 
+    {
+        search: "",
+        userID: userID
+    }
+
+    let jsonPayload = JSON.stringify(temp);
+
+    let url = urlBase + '/SearchContact.' + extension;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try
+    {
+        xhr.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                let jsonObject = JSON.parse(xhr.responseText);
+                if (jsonObject.error)
+                {
+                    console.log(jsonObject.error);
+                    return;
+                }
+
+                let text = "<table border = '1'>"
+                for (let i = 0; i < jsonObject.results.length; i++)
+                {
+                    let first = jsonObject.results[i].firstName;
+                    let last = jsonObject.results[i].lastName;
+                    let email = jsonObject.results[i].email;
+                    let phone = jsonObject.results[i].phone;
+                    console.log(phone);
+
+                    ids[i] = jsonObject.results[i].ID;
+                    text += "<tr id='row" + i + "'>"
+                    text += "<td id='first_Name" + i + "'><span>" + first + "</span></td>";
+                    text += "<td id='last_Name" + i + "'><span>" + last + "</span></td>";
+                    text += "<td id='email" + i + "'><span>" + email + "</span></td>";
+                    text += "<td id='phone" + i + "'><span>" + phone + "</span></td>";
+                    text += "<td id='updateBtn" + i + " '<button type='button' class='btn-primary'>Update</button> </span></td>";
+                    text += "<td id='updateBtn" + i + " '<button type='button' class='btn-primary'>Delete</button> </span></td>";
+                    text += "<tr/>"
+                }
+                //console.log(typeof text);
+                text += "</table>";
+                document.getElementById("tbody").innerHTML = text;
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch (err)
+    {
+        console.log(err.message);
+    }
+}
+
+function showTable() {
+    var x = document.getElementById("addContact");
+    var contacts = document.getElementById("contactsTable")
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+        contacts.style.display = "none";
+    } else 
+    {
+        x.style.display = "none";
+        contacts.style.display = "block";
+    }
+}
+
+
+function addContact() 
+{
     console.log("Add contact button clicked")
     let firstName = document.getElementById("firstName").value;
     let lastName = document.getElementById("lastName").value;
-    let userID = document.getElementById("userID").value
     let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
-    let errorMsg = document.getElementById("errorMessage");
+    let phone = document.getElementById("phoneNumber").value;
+    //let errorMsg = document.getElementById("errorMessage");
+
+    if (!(checkValidEmail(email) && checkValidPhone(phone))) 
+    {
+        console.log("INVALID EMAIL OR PHONE SUBMISSION");
+        return;
+    }
 
     let tmp = { "firstName": firstName, "lastName": lastName, "email": email, "phone": phone, "userID": userID };
     let jsonPayload = JSON.stringify(tmp);
     let url = urlBase + '/AddContact.' + extension;
 
-    if (firstName == "") {
-        errorMsg.innerHTML = "First name is required";
-        return;
-    }
-    if (lastName == "") {
-        errorMsg.innerHTML = "Last name is required";
-        return;
-    }
-    if (email == "" && phone == "") {
-        errorMsg.innerHTML = "Must add phone, email, or both";
-        return;
-    }
+    // if (firstName == "") 
+    // {
+    //     errorMsg.innerHTML = "First name is required";
+    //     return;
+    // }
+    // if (lastName == "") 
+    // {
+    //     errorMsg.innerHTML = "Last name is required";
+    //     return;
+    // }
+    // if (email == "" && phone == "") 
+    // {
+    //     errorMsg.innerHTML = "Must add phone, email, or both";
+    //     return;
+    // }
 
-    if (!(checkValidEmail() && checkValidPhone())) {
-        return;
-    }
+    
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try {
+    try 
+    {
         xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                let jsonObject = JSON.parse(xhr.responseText);
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                console.log("Contact has been added!");
 
-                if (jsonObject.error == "") {
-                    document.getElementById("addContactResult").innerHTML = `Successfully added ${document.getElementById("firstName")} ${document.getElementById("lastName")} to contacts`;
+                loadContact();
+                showTable();
+                let jsonObject = JSON.parse(xhr.responseText);
+                if (jsonObject.error == "") 
+                {
+                    //document.getElementById("addContactResult").innerHTML = `Successfully added ${document.getElementById("firstName")} ${document.getElementById("lastName")} to contacts`;
                     $('#addContactWindow').modal('hide')
                 }
-                else {
+                else 
+                {
                     document.getElementById("addContactResult").innerHTML = jsonObject.error
                     $('#addContactWindow').modal('hide')
                     return
                 }
 
-                //saveCookie();
+
+                saveCookie();
             }
         };
         xhr.send(jsonPayload);
     }
-    catch (err) {
-        document.getElementById("addContactResult").innerHTML = err.message
+    catch (err) 
+    {
+        console.log(err.message);
     }
 }
 
-function checkValidEmail() {
-    email = document.getElementById("email").value;
-    if (!(/(\w+)@(\w+).[a-z]/.test(email))) {
+function checkValidEmail(email) {
+    // email = document.getElementById("email").value;
+    var regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    if (regex.test(email) == false) {
         document.getElementById("errorMessage").innerHTML = "Invalid email format (sample email: 'xyz@domain.com')"
         return false;
     }
@@ -79,9 +172,11 @@ function checkValidEmail() {
     return true;
 
 }
-function checkValidPhone() {
-    phone = document.getElementById("phone").value;
-    if (!(/^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$/).test(phone)) {
+function checkValidPhone(phone) {
+    // phone = document.getElementById("phone").value;
+    var regex = /^[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/;
+
+    if (regex.test(phone) == false) {
         document.getElementById("errorMessage").innerHTML = "Invalid phone number (sample phone: '123-456-7890')";
         return false;
     }
@@ -90,7 +185,7 @@ function checkValidPhone() {
 }
 
 function readCookie() {
-    userId = -1
+    userID = -1
     let data = document.cookie
     let splits = data.split(",");
     for (var i = 0; i < splits.length; i++) {
@@ -103,11 +198,11 @@ function readCookie() {
             lastName = tokens[1]
         }
         else if (tokens[0] == "userId") {
-            userId = parseInt(tokens[1].trim());
+            userID = parseInt(tokens[1].trim());
         }
     }
 
-    if (userId < 0) {
+    if (userID < 0) {
         window.location.href = "index.html";
     }
     else {
@@ -116,8 +211,8 @@ function readCookie() {
 }
 
 function saveCookie(){
-        let minutes = 20;
-        let date = new Date();
-        date.setTime(date.getTime()+(minutes*60*1000));
-        document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userID + ";expires=" + date.toGMTString();
 }
